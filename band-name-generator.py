958d60b544215c0ef299360950e7e6c558e9c358
Fernando Benitez <fernando.benitezai@gmail.com>
2026-05-05 21:25:21 -0600

    Removed duplicate file

--- band-name-generator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome To The Band NAme Generator.")
-city = input("What´s the name of the city you grew up in?\n ")
-pet = input("What´s your pet´s name?\n")
-print("Your band name could be " + city + " " + pet)
-
-print("Do you wanna be my girlfriend? ")
-girlfriend = input()
