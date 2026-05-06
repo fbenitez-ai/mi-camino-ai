9531f2837a6fad7d21a1a265081b73e7ab159db9
Fernando Benitez <fernando.benitezai@gmail.com>
2026-05-05 21:14:37 -0600

    Dia 1 terminado y Dia 2 en progreso

---
+++ DIA_2_POR_TERMINAR.py
@@ -0,0 +1,22 @@
+# subscripting
+print("Hello"[-5])
+
+# string
+print("123" + "456")
+
+# integer
+print(123 + 345)
+
+#large Integers
+print(123_456_789)
+
+#float = Floating point number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
+
+
+
