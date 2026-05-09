8520f51ac63eedf4192a1ecadf984b7dd5d1f5c8
Fernando Benitez <fernando.benitezai@gmail.com>
2026-05-08 23:01:39 -0600

    Day 2 - Roallercoaster proyect

---
+++ day2_rollercoaster_proyect.py
@@ -0,0 +1,23 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height == 120:
+    print("Your can ride the roller coaster")
+else:
+    print("Sorry you have to grow taller before you can ride")
+
+
+#saldo = int(input("¿Cuál es tu saldo? "))
+#retiro = int(input("¿Cuánto quieres retirar? "))
+
+#if retiro <= 0:
+#   print("Cantidad inválida")
+#elif retiro > saldo:
+#   print("Saldo insuficiente")
+#elif retiro == saldo:
+#   print("Retiraste todo tu dinero")
+#else:
+    #print(f"Retiro exitoso. Tu saldo restante es: {saldo - retiro}")
+
+
+
